4a161c2b829f23a80ab9bb538d772b39b565dd78
lamersc <me@lamersc.com>
2026-02-28 06:52:04 +0000

    implemented basic paginator

--- lightskyblue/socialdistribution/api/views.py
+++ lightskyblue/socialdistribution/api/views.py
@@ -3,7 +3,9 @@
 # data from or to the database, so that they can be reused in the user-facing
 # views.
 from http.client import HTTPResponse
+from urllib.request import Request
 
+from django.core.paginator import Paginator
 from django.http import JsonResponse
 from django.views import View
 from django.views.generic import ListView
@@ -11,16 +13,37 @@ from django.forms.models import model_to_dict
 from ..models import Author
 
 
-class AuthorsView(ListView):
+# per https://uofa-cmput404.github.io/general/project.html#authors-api
+class AuthorsView(View):
 
     def _pull(self):
         return Author.objects.all()
 
-    def get(self, req):
-        data = self._pull().last().serialize()
-        #print(data.user.username)
-        return JsonResponse(data, safe=False)
+    def get(self, req: HTTPResponse):
+        authors = self._pull()
+        try:
+            page = req.GET.get('page', 1)
+        except ValueError:
+            page = 1
+        try:
+            size = req.GET.get('size', 10)
+        except ValueError:
+            size = 5
 
+        # note: gpt-5.1-codex-mini(medium) taught me about the paginator
+        # ref: https://docs.djangoproject.com/en/6.0/topics/pagination/
+        paginator = Paginator(authors, size)
+        page = paginator.get_page(page)
+
+        resp = {}
+        resp["type"] = "authors"
+        resp["authors"] = []
+        for author in page.object_list:
+            resp["authors"].append(author.serialize())
+
+        return JsonResponse(resp)
+
+# per https://uofa-cmput404.github.io/general/project.html#single-author-api
 class AuthorAPI(View):
     def _pull(self, author_id):
         return Author.objects.get(id=author_id)
