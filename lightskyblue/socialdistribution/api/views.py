4941409dab4e54ec5e59e3b9b263ddb1d5f7b57d
lamersc <me@lamersc.com>
2026-03-02 04:58:34 +0000

    added comments to implemented api points

--- lightskyblue/socialdistribution/api/views.py
+++ lightskyblue/socialdistribution/api/views.py
@@ -1,10 +1,11 @@
-# note:
-# all apis must be written in an extensible fashion when pulling or pushing
-# data from or to the database, so that they can be reused in the user-facing
-# views.
+"""
+The API's try to follow a `pull` and `push` methodology, with `pull`ing retrieving information
+from an API's relevant model, and `push`ing updating the API's relevant model.
+"""
 from functools import wraps
 from http.client import HTTPResponse
 import json
+from django.contrib.auth.models import User
 from urllib.request import Request
 
 from django.core.paginator import Paginator
@@ -17,6 +18,15 @@ from ..models import Author, FollowRequest
 
 
 def get_author_model_from_id(author_id: int | str) -> Author | None:
+    """
+    Retrieves an `Author` from the database based on the provided author ID.
+    Supports either number ID or the authors display name.
+
+    :param author_id: Either the number ID or the display name of the author.
+    :type author_id: int | str
+    :return: The `Author` if found; otherwise, returns `None`.
+    :rtype: Author | None
+    """
     try:
         if author_id.isdigit():
             return Author.objects.get(id=int(author_id))
@@ -25,15 +35,25 @@ def get_author_model_from_id(author_id: int | str) -> Author | None:
     except ObjectDoesNotExist:
         return None
 
-def user_is_author(user, author: Author) -> bool:
-    return user.id == author.id
-
 
 # ref: https://deepankarm.github.io/posts/type-safe-python-decorators/
 # the purpose of this decorator is to make sure the user is authorized to access / modify
 # the information at the route.
 # this works elegantly, but still seems a bit like dark magic to me lol
 def user_must_be_author(capture_name: str):
+    """
+    Decorator used to reduce the amount of boilerplate to ensure the requesting
+    user has authorization to view / modify a given authors model.
+
+    Usage example::
+
+        @user_must_be_author(capture_name="author_id")
+        def get(self, req: HTTPResponse):
+            ...
+
+    :param capture_name: Captured values name in the route that represents the author ID
+    :type capture_name: str
+    """
     def decorator(func):
         @wraps(func)
         def wrapper(self, *args, **kwargs):
@@ -49,11 +69,16 @@ def user_must_be_author(capture_name: str):
         return wrapper
     return decorator
 
-# per https://uofa-cmput404.github.io/general/project.html#inbox-api
-# likely the most important api of our project (thus why its at the top).
-# it's very multi-faceted, taking on roles from other apis as per spec
-class AuthorInboxAPI(View):
 
+class AuthorInboxAPI(View):
+    """
+    Route: /authors/{AUTHOR_SERIAL}/inbox
+    Implements: https://uofa-cmput404.github.io/general/project.html#inbox-api
+    Complete?: No
+
+    Likely the most important api of our project (thus why it's at the top).
+    It's very multi-faceted, taking on roles from other apis as per spec.
+    """
     # NOTE: the authentication used here is NOT CORRECT yet. We
     # need to figure out a way to verify which nodes are allowed
     # to post to our nodes inbox
@@ -80,9 +105,12 @@ class AuthorInboxAPI(View):
         return JsonResponse(author.serialize(), safe=True)
 
 
-# per https://uofa-cmput404.github.io/general/project.html#authors-api
 class AuthorsAPI(View):
-
+    """
+    Route: /authors
+    Implements: https://uofa-cmput404.github.io/general/project.html#authors-api
+    Complete?: Pending
+    """
     def _pull(self):
         return Author.objects.all()
 
@@ -113,9 +141,12 @@ class AuthorsAPI(View):
 
         return JsonResponse(resp)
 
-# per https://uofa-cmput404.github.io/general/project.html#single-author-api
 class AuthorAPI(View):
-
+    """
+    Route: /author/{AUTHOR_SERIAL}/
+    Implements: https://uofa-cmput404.github.io/general/project.html#single-author-api
+    Complete?: Pending
+    """
     def _pull(self, author_id: int | str) -> Author | None:
         return get_author_model_from_id(author_id)
 
@@ -141,7 +172,11 @@ class AuthorAPI(View):
 
 # per https://uofa-cmput404.github.io/general/project.html#following-api
 class AuthorFollowingsAPI(View):
-
+    """
+    Route: /authors/{AUTHOR_SERIAL}/following
+    Implements: https://uofa-cmput404.github.io/general/project.html#following-api
+    Complete?: Pending
+    """
     def _pull(self, author_id: int | str) -> Author | None:
         return get_author_model_from_id(author_id)
 
@@ -156,10 +191,12 @@ class AuthorFollowingsAPI(View):
             resp["authors"].append(author.serialize())
         return JsonResponse(resp)
 
-# per https://uofa-cmput404.github.io/general/project.html#following-api (foreign authors
-# TODO: implement
 class AuthorFollowingPerUserAPI(View):
-
+    """
+    Route: /authors/{AUTHOR_SERIAL}/following/{FOREIGN_AUTHOR_FQID}
+    Implements: https://uofa-cmput404.github.io/general/project.html#following-api
+    Complete?: Pending
+    """
     def _pull(self, author_id: int | str) -> Author | None:
         return get_author_model_from_id(author_id)
 
@@ -176,10 +213,12 @@ class AuthorFollowingPerUserAPI(View):
         return JsonResponse(target_author.serialize(), safe=True)
 
 
-
-# per https://uofa-cmput404.github.io/general/project.html#followers-api
 class AuthorFollowersAPI(View):
-
+    """
+    Route: /authors/{AUTHOR_SERIAL}/followers/{FOREIGN_AUTHOR_FQID}
+    Implements: https://uofa-cmput404.github.io/general/project.html#followers-api
+    Complete?: Pending
+    """
     def _pull(self, author_id: int | str) -> Author | None:
         return get_author_model_from_id(author_id)
 
@@ -199,6 +238,11 @@ class AuthorFollowersAPI(View):
 
 # per https://uofa-cmput404.github.io/general/project.html#follow-request-api
 class AuthorFollowRequestAPI(View):
+    """
+    Route: /authors/{AUTHOR_SERIAL}/follow_requests
+    Implements: https://uofa-cmput404.github.io/general/project.html#follow-request-api
+    Complete?: Pending
+    """
     def _pull(self, author_id: int | str) -> Author | None:
         return get_author_model_from_id(author_id)
 
