afe0d51de976c02dd9f76e9c184f98ebb9d7d70f
lamersc <me@lamersc.com>
2026-03-03 20:58:14 +0000

    rebuilding api

--- lightskyblue/socialdistribution/api/tests.py
+++ lightskyblue/socialdistribution/api/tests.py
@@ -1,159 +1,253 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from ..models import Author
 
-class APITests:
+
+# disclaimer of AI usage:
+# many of these test cases are initially generated with Kimi 2.5, then fine-tuned
+# and cleaned uo by hand
+class APITests(TestCase):
+    user_model = get_user_model()
 
     def setUp(self):
-        pass
+        self.test_user = self.user_model.objects.create(
+            username="testuser",
+            password="password",
+        )
+        self.test_author = Author.objects.create(
+            user=self.test_user,
+            display_name="Test Author",
+            host="https://testauthor/",
+            bio="A test author.",
+            github_url="https://github.com/veryfaketestauthorsgithub",
+            picture_url="http://example.com/testauthorsphoto.jpg",
+        )
+
+        self.client.force_login(self.test_user)
 
     def tearDown(self):
         pass
 
-
     """
     REGION https://uofa-cmput404.github.io/general/project.html#authors-api
     """
-    def api_authors_get(self):
+
+    def test_api_authors_get(self):
         """
         Test: GET /api/authors/
         Targets: local, remote
         """
+        # Create multiple authors for pagination testing
+        authors = []
+        for i in range(5):
+            user = self.user_model.objects.create_user(
+                username=f"testuser{i}",
+                password=f"testpass{i}",
+            )
+            author = Author.objects.create(
+                user=user,
+                display_name=f"Test Author {i}",
+                host="http://testserver",
+                bio=f"Bio for author {i}",
+                github_url=f"https://github.com/testuser{i}",
+                picture_url=f"http://example.com/pic{i}.jpg",
+            )
+            authors.append(author)
+            self.addCleanup(user.delete)
+            self.addCleanup(author.delete)
+
+        # Test basic GET request
+        response = self.client.get("/api/authors/")
+        self.assertEqual(response.status_code, 200)
+
+        payload = response.json()
+        self.assertEqual(payload["type"], "authors")
+        self.assertIn("authors", payload)
+        self.assertIn("page", payload)
+        self.assertIn("size", payload)
+
+        # Check that all authors are returned (including existing ones)
+        all_authors_count = Author.objects.count()
+        self.assertEqual(
+            len(payload["authors"]), min(all_authors_count, payload["size"])
+        )
+
+        # Check author structure matches spec
+        if payload["authors"]:
+            author_data = payload["authors"][0]
+            self.assertEqual(author_data["type"], "author")
+            self.assertIn("id", author_data)
+            self.assertIn("host", author_data)
+            self.assertIn("displayName", author_data)
+            self.assertIn("github", author_data)
+            self.assertIn("profileImage", author_data)
+            self.assertIn("web", author_data)
+
+        # Test pagination with page and size parameters
+        response = self.client.get("/api/authors/?page=1&size=2")
+        self.assertEqual(response.status_code, 200)
+
+        payload = response.json()
+        self.assertEqual(payload["type"], "authors")
+        self.assertEqual(payload["page"], 1)
+        self.assertEqual(payload["size"], 2)
+        self.assertEqual(len(payload["authors"]), 2)
+
+        # Test second page
+        response = self.client.get("/api/authors/?page=2&size=2")
+        self.assertEqual(response.status_code, 200)
+
+        payload = response.json()
+        self.assertEqual(payload["page"], 2)
+        self.assertEqual(len(payload["authors"]), 2)
+
     """
     ENDREGION
     """
-
-
 
     """
     REGION https://uofa-cmput404.github.io/general/project.html#single-author-api
     """
+
     def api_authors_の_get(self):
         """
         Test: GET /api/authors/の
         """
+
     def api_authors_の_put(self):
         """
         Test: PUT /api/authors/の
         """
+
     """
     ENDREGION
     """
-
-
 
     """
     REGION https://uofa-cmput404.github.io/general/project.html#following-api
     """
+
     def api_authors_の_following_get(self):
         """
         Test: GET api/authors/の/following
         """
+
     def api_authors_の_following_ゟ_get(self):
         """
         Test: GET api/authors/の/following/ゟ
         """
+
     def api_authors_の_following_ゟ_put(self):
         """
         Test: PUT api/authors/の/following/ゟ
         """
+
     def api_authors_の_following_ゟ_delete(self):
         """
         Test: DELETE api/authors/の/following/ゟ
         """
+
     """
     ENDREGION
     """
-
-
 
     """
     REGION https://uofa-cmput404.github.io/general/project.html#followers-api
     """
+
     def api_authors_の_followers_ゟ_get(self):
         """
         Test: GET api/authors/の/followers/ゟ
         """
+
     def api_authors_の_followers_ゟ_put(self):
         """
         Test: PUT api/authors/の/followers/ゟ
         """
+
     def api_authors_の_followers_ゟ_delete(self):
         """
         Test: DELETE api/authors/の/followers/ゟ
         """
+
     """
     ENDREGION
     """
-
-
 
     """
     REGION https://uofa-cmput404.github.io/general/project.html#follow-request-api
     """
+
     def api_authors_の_followくrequests_get(self):
         """
         Test: GET api/authors/の/follow_requests
         """
+
     def api_authors_の_followくrequests_post(self):
         """
         Test: POST api/authors/の/inbox
         """
+
     """
     ENDREGION
     """
-
-
 
     """
     REGION https://uofa-cmput404.github.io/general/project.html#entries-api
     """
+
     def api_authors_の_entries_ゟ_get(self):
         """
         Test: GET api/authors/の/entries/ゟ
         """
+
     def api_authors_の_entries_ゟ_put(self):
         """
         Test: PUT api/authors/の/entries/ゟ
         """
+
     def api_authors_の_entries_ゟ_delete(self):
         """
         Test: DELETE api/authors/の/entries/ゟ
         """
+
     def api_entries_ゟ_get(self):
         """
         Test: GET api/entries/ゟ
         """
+
     def api_authors_の_entries_get(self):
         """
         Test: GET api/authors/の/entries
         """
+
     def api_authors_の_entries_post(self):
         """
         Test: POST api/authors/の/entries
         """
+
     """
     ENDREGION
     """
-
-
 
     """
     REGION https://uofa-cmput404.github.io/general/project.html#image-entries
     """
+
     def api_authors_の_entries_ゟ_image_get(self):
         """
         Test: GET api/authors/の/entries/ゟ/image
         """
+
     def api_entries_の_image_get(self):
         """
         Test: GET api/entries/の/image
         """
+
     """
     ENDREGION
     """
-
-
 
     """
     REGION https://uofa-cmput404.github.io/general/project.html#comments-api
@@ -163,8 +257,6 @@ class APITests:
     ENDREGION
     """
 
-
-
     """
     REGION https://uofa-cmput404.github.io/general/project.html#commented-api
     """
@@ -172,8 +264,6 @@ class APITests:
     """
     ENDREGION
     """
-
-
 
     """
     REGION https://uofa-cmput404.github.io/general/project.html#likes-api
@@ -183,8 +273,6 @@ class APITests:
     ENDREGION
     """
 
-
-
     """
     REGION https://uofa-cmput404.github.io/general/project.html#liked-api
     """
@@ -192,8 +280,6 @@ class APITests:
     """
     ENDREGION
     """
-
-
 
     """
     REGION https://uofa-cmput404.github.io/general/project.html#inbox-api
--- lightskyblue/socialdistribution/api/urls.py
+++ lightskyblue/socialdistribution/api/urls.py
@@ -1,9 +1,16 @@
 # ref: https://docs.djangoproject.com/en/6.0/topics/http/urls/
 
-from django.urls import re_path, path, include
-from . import views
-from .views import AuthorsAPI, AuthorAPI, AuthorFollowingsAPI, AuthorFollowersAPI, AuthorFollowingPerUserAPI, \
-    AuthorFollowRequestAPI
+from django.urls import path
+
+from .views import api_authors
+from .views_old import (
+    AuthorAPI,
+    AuthorFollowersAPI,
+    AuthorFollowingPerUserAPI,
+    AuthorFollowingsAPI,
+    AuthorFollowRequestAPI,
+    AuthorsAPI,
+)
 
 # https://regex101.com/r/1tqPOL/1 (matches either author id or name)
 author_id_regex = r"(?P<author_id>[\w\s]+)"
@@ -12,25 +19,5 @@ target_author_id_regex = r"(?P<target_author_id>[\w\s]+)"
 urlpatterns = [
     # very rough fix for supporting both regular and
     # directory-based paths
-    path("authors", include(
-        [
-            path("", AuthorsAPI.as_view()),
-            re_path(author_id_regex, include(
-                [
-                    path("", AuthorAPI.as_view()),
-                    path("/", include([
-                        #path("following", AuthorFollowingsAPI.as_view()),
-                        path("following", include(
-                            [
-                                path("", AuthorFollowingsAPI.as_view()),
-                                re_path(target_author_id_regex, AuthorFollowingPerUserAPI.as_view()),
-                        ])),
-                        path("followers", AuthorFollowersAPI.as_view()),
-                        path("follow_requests", AuthorFollowRequestAPI.as_view()),
-                    ]))
-                ]
-            )),
-
-        ]
-    )),
+    path("authors/", api_authors.as_view())
 ]
